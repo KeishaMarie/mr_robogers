a0e1573557392fb3565c10699846527196b0646c
Keisha Hepner <kmhepner90@gmail.com>
2022-08-21 12:14:21 -0700

    Test to only allow numbers to be entered successful

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,4 @@
 
-window.onload = function() {
-  let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
-    const userInput = document.getElementById("userInput").value;
-    document.querySelector("span#messageOutput").innerText = userInput;
-  }
-}
-
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const userInput = document.getElementById("userInput").value;
-  document.querySelector("span#messageOutput").innerText = userInput; 
-}
-
-window.addEventListener("load", function() {
-  document.querySelector("form#numberToWord").addEventListener("submit", handleFormSubmission);
-});
-
 // Business Logic
 
 function createArray (userInput) {
@@ -25,12 +6,12 @@ function createArray (userInput) {
   return Array.from(keys);
 };
 
-x = createArray()
-for (let element of x) {
-  numberResponse = messageOutput (element)
-};
+// x = createArray()
+// for (let element of x) {
+//   numberResponse = messageOutput (element)
+// };
 
-function messageOutput (number) {
+function createMessageOutput (number) {
   const number_string = number.toString();
 
     if (number_string.includes("3")) {
@@ -43,3 +24,36 @@ function messageOutput (number) {
       else return number_string;
   };
 
+  //UI Logic
+  
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    const userInput = document.querySelector("input#userInput").value;
+    userInput = parseInt(userInput)
+    console.log(userInput)
+    userArray = createArray(userInput);
+    console.log(userArray)
+    newUserArray = []
+    for (let element of userArray ) {
+      elementResponse = createMessageOutput (element)
+      newUserArray.push(elementResponse)
+    };
+    messageOutput = newUserArray.join(", ");
+    // let messageOutput;
+    // messageOutput = createMessageOutput (userInput);
+    document.getElementById("messageOutput").innerText = messageOutput;
+  };
+};
+
+// function handleFormSubmission(event) {
+//   event.preventDefault();
+//   const userInput = document.getElementById("userInput").value;
+//   document.querySelector("span#messageOutput").innerText = messageOutput; 
+// }
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("numberToWord")
+  form.addEventListener("submit", createMessageOutput);
+});
